e8d46e330b499964056019647132f4ac038e377c
parkUiery <parkuiery@gmail.com>
2023-10-16 23:40:28 +0900

    🌧 :: 로그인 api 추가

--- SVAP_Android/app/src/main/java/com/amazing/android/svap_android/api/AuthAPI.kt
+++ SVAP_Android/app/src/main/java/com/amazing/android/svap_android/api/AuthAPI.kt
@@ -1,4 +1,15 @@
 package com.amazing.android.svap_android.api
 
+import com.amazing.android.svap_android.login.LoginRequest
+import com.amazing.android.svap_android.login.LoginResponse
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface AuthAPI {
+
+    @POST("/login")
+    fun login(
+        @Body request: LoginRequest
+    ): Call<LoginResponse>
 }
