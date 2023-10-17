583c60018141a28096649135f5a626abbf071ff3
parkUiery <parkuiery@gmail.com>
2023-10-17 10:45:03 +0900

    🌱 :: api 생성

--- SVAP_Android/app/src/main/java/com/amazing/android/svap_android/api/ApiProvider.kt
+++ SVAP_Android/app/src/main/java/com/amazing/android/svap_android/api/ApiProvider.kt
@@ -1,4 +1,34 @@
 package com.amazing.android.svap_android.api
 
-class ApiProvider {
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object ApiProvider {
+    private var instance: Retrofit? = null
+    private const val CONNECT_TIMEOUT_SEC = 20000L
+
+    fun getInstance(): Retrofit {
+        if (instance == null) {
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .connectTimeout(CONNECT_TIMEOUT_SEC, TimeUnit.SECONDS)
+                .build()
+
+            instance = Retrofit.Builder()
+                .baseUrl("")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+        }
+        return instance!!
+
+    }
+
+    fun getAuthApi() = getInstance().create(AuthAPI::class.java)
 }
