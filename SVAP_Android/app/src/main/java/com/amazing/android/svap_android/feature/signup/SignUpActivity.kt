7211bf2c505c702ef1df833ee435476f242891b2
parkUiery <parkuiery@gmail.com>
2023-11-07 19:28:40 +0900

    🌧 :: 회원 가입 화면 이동

--- SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/signup/SignUpActivity.kt
+++ SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/signup/SignUpActivity.kt
@@ -1,11 +1,32 @@
-package com.amazing.android.svap_android
+package com.amazing.android.svap_android.feature.signup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.amazing.android.svap_android.R
+import com.amazing.android.svap_android.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initFragmentManager();
+    }
+
+    private fun initFragmentManager() {
+        //val fragmentManger : FragmentManager = supportFragmentManager
+        val fragmentTransaction : FragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fl_signup, SignUpIdFragment());
+        fragmentTransaction.commit()
     }
 }
+
+
