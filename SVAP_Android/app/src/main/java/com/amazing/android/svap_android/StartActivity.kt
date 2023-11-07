6deb6c0156042102f5df3cfed296175e7a4364ce
parkUiery <parkuiery@gmail.com>
2023-11-08 07:34:40 +0900

    🌧 :: 시작 화면

--- SVAP_Android/app/src/main/java/com/amazing/android/svap_android/StartActivity.kt
+++ SVAP_Android/app/src/main/java/com/amazing/android/svap_android/StartActivity.kt
@@ -2,10 +2,14 @@ package com.amazing.android.svap_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.amazing.android.svap_android.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStartBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+
+        val binding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
