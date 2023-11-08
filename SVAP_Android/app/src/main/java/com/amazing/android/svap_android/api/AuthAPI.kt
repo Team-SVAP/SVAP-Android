0c1c2038045907a539016be40d6c433638c1ebec
parkUiery <parkuiery@gmail.com>
2023-11-08 09:23:43 +0900

    🌧 :: 회원 가입 비번 api 추가

--- SVAP_Android/app/src/main/java/com/amazing/android/svap_android/api/AuthAPI.kt
+++ SVAP_Android/app/src/main/java/com/amazing/android/svap_android/api/AuthAPI.kt
@@ -2,14 +2,20 @@ package com.amazing.android.svap_android.api
 
 import com.amazing.android.svap_android.feature.login.LoginRequest
 import com.amazing.android.svap_android.feature.login.LoginResponse
+import com.amazing.android.svap_android.feature.signup.signupId.CkPasswordRequest
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthAPI {
 
-    @POST("/login")
+    @POST("/login") //로그인
     fun login(
         @Body request: LoginRequest
     ): Call<LoginResponse>
+
+    @POST("/ck-password") //회원가입 비번
+    fun ckPassword(
+        @Body request: CkPasswordRequest
+    ): Call<Void>
 }
