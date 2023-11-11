f0c1e90398f3b6b16fcc84de37e8675df70b1229
parkUiery <parkuiery@gmail.com>
2023-11-11 18:43:16 +0900

    🌧 :: 네비게이션 추가

--- SVAP_Android/app/src/main/java/com/amazing/android/svap_android/MainActivity.kt
+++ SVAP_Android/app/src/main/java/com/amazing/android/svap_android/MainActivity.kt
@@ -2,10 +2,57 @@ package com.amazing.android.svap_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import com.amazing.android.svap_android.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListener{
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initNavigationMenu()
+    }
+
+    private fun initNavigationMenu() {
+        val drawerLayout = binding.mainDrawerLayout
+        val navView = binding.mainNavigationView
+
+        navView.setNavigationItemSelectedListener(this)
+
+        val navMenu = binding.tbMain.ibToolBarMenu.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        val headerView = navView.getHeaderView(0)
+        val closeBtn = headerView.findViewById<ImageView>(R.id.iv_header_close)
+        closeBtn.setOnClickListener {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }
+    }
+
+    //메인 액션바 옵션 설정 함수
+    //툴바 세팅
+    /*override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_nav_menu_list, menu)
+        //커스텀 메뉴
+        val settingIcon = menu?.findItem(R.id.iv_main_nav_header_setting)
+        return super.onCreateOptionsMenu(menu)
+    }*/
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.menu_item1 -> Toast.makeText(this, "메뉴1", Toast.LENGTH_SHORT).show()
+        }
+        return false
     }
 }
