97c50e77d3293a486fe7ec8fa5eb9502cdcc16c6
parkUiery <parkuiery@gmail.com>
2023-11-12 13:21:35 +0900

    🌱 :: 내가 쓴 청원 페이지

---
+++ SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/myPetition/MyPetitionActivity.kt
@@ -0,0 +1,12 @@
+package com.amazing.android.svap_android.feature.myPetition
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.amazing.android.svap_android.R
+
+class MyPetitionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_petition)
+    }
+}
