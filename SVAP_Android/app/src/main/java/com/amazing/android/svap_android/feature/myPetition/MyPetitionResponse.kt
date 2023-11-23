b91db4f6e33c5ddddead70e36f98d4f9149342a8
parkUiery <parkuiery@gmail.com>
2023-11-23 23:12:09 +0900

    🌧 :: 내 청원 api 응답

--- SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/myPetition/MyPetitionResponse.kt
+++ SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/myPetition/MyPetitionResponse.kt
@@ -1,4 +1,13 @@
 package com.amazing.android.svap_android.feature.myPetition
 
-class MyPetitionResponse {
-}
+import com.amazing.android.svap_android.type.Type
+import java.time.LocalDateTime
+
+data class MyPetitionResponse (
+    val id: String,
+    val title: String,
+    val content: String,
+    val dateTime: LocalDateTime,
+    val types: Type,
+    val location: String,
+)
