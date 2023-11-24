54129bc6869113d612cd60e27f1878cd625f4119
parkUiery <parkuiery@gmail.com>
2023-11-24 22:26:40 +0900

    🌧 :: api 수정

--- SVAP_Android/app/src/main/java/com/amazing/android/svap_android/api/UserApi.kt
+++ SVAP_Android/app/src/main/java/com/amazing/android/svap_android/api/UserApi.kt
@@ -2,6 +2,7 @@ package com.amazing.android.svap_android.api
 
 import com.amazing.android.svap_android.feature.login.LoginRequest
 import com.amazing.android.svap_android.feature.login.LoginResponse
+import com.amazing.android.svap_android.feature.myPetition.MyPetitionResponse
 import com.amazing.android.svap_android.feature.signup.SignUpRequest
 import com.amazing.android.svap_android.feature.signup.SignUpResponse
 import com.amazing.android.svap_android.feature.signup.signupId.SignUpIdRequest
@@ -9,6 +10,8 @@ import com.amazing.android.svap_android.feature.signup.signupName.SignUpNameRequ
 import com.amazing.android.svap_android.feature.signup.signupPw.SignUpPwRequest
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface UserApi {
@@ -38,4 +41,8 @@ interface UserApi {
         @Body request: SignUpRequest
     ): Call<SignUpResponse>
 
+    @GET("/user/") //내가 쓴 청원 보기
+    fun showMyPetition(
+        @Header("Authorization") accessToken: String
+    ): Call<List<MyPetitionResponse>>
 }
