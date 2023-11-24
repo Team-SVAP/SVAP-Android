097950e0c8b4facfbe7e80776223b16d956a0a77
parkUiery <parkuiery@gmail.com>
2023-11-24 22:29:08 +0900

    🌱 :: 내 청원 리사이클러뷰 어댑터

--- SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/myPetition/MyPetitionAdapter.kt
+++ SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/myPetition/MyPetitionAdapter.kt
@@ -1,4 +1,39 @@
 package com.amazing.android.svap_android.feature.myPetition
 
-class MyPetitionAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.amazing.android.svap_android.R
+
+class MyPetitionAdapter(private val itemList: List<MyPetitionResponse>) :
+    RecyclerView.Adapter<MyPetitionAdapter.MyPetitionViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MyPetitionAdapter.MyPetitionViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.my_petition_item, parent, false)
+        return MyPetitionViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyPetitionViewHolder, position: Int) {
+        holder.title.text = itemList[position].title
+        holder.date.text = itemList[position].dateTime.toString()
+        holder.content.text = itemList[position].content
+        holder.tag.text = itemList[position].location
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.count()
+    }
+
+    inner class MyPetitionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val title: TextView = itemView.findViewById(R.id.tv_my_petition_item_title)
+        val date: TextView = itemView.findViewById(R.id.tv_my_petition_item_date)
+        val content: TextView = itemView.findViewById(R.id.tv_my_petition_item_content)
+        val tag: TextView = itemView.findViewById(R.id.tv_my_petition_item_tag)
+    }
 }
