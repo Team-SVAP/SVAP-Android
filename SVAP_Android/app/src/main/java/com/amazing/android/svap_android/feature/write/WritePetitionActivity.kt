f263247e763b39579395a7f5e75f94167a501ea3
parkUiery <parkuiery@gmail.com>
2023-11-25 00:06:49 +0900

    🌱 :: 청원 작성 페이지

---
+++ SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/write/WritePetitionActivity.kt
@@ -0,0 +1,12 @@
+package com.amazing.android.svap_android.feature.write
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.amazing.android.svap_android.R
+
+class WritePetitionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write_petition)
+    }
+}
