74a2d6b1cc1fb413592839c531ed94652b2d0b7e
parkUiery <parkuiery@gmail.com>
2023-11-25 18:55:20 +0900

    🌱 :: 타입 스피너 어댑터 추가

--- SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/write/TypeSpinnerAdapter.kt
+++ SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/write/TypeSpinnerAdapter.kt
@@ -1,4 +1,37 @@
 package com.amazing.android.svap_android.feature.write
 
-class TypeSpinnerAdapter {
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.annotation.LayoutRes
+import com.amazing.android.svap_android.databinding.PetitionTypeItemBinding
+
+class TypeSpinnerAdapter(
+    context: Context,
+    @LayoutRes private val resId: Int,
+    private val values: MutableList<PetitionTypeModel>
+) : ArrayAdapter<PetitionTypeModel>(context,resId,values){
+
+    override fun getCount() = values.size
+
+    override fun getItem(position: Int) = values[position]
+
+    @SuppressLint("ViewHolder")
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val binding = PetitionTypeItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val model = values[position]
+        binding.tvPetitionType.text = model.name
+        return binding.root
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val binding = PetitionTypeItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val model = values[position]
+
+        binding.tvPetitionType.text = model.name
+        return binding.root
+    }
 }
