0b0426f8c595095408a27d1a22e6f5c3601a562b
parkUiery <parkuiery@gmail.com>
2023-11-28 15:30:06 +0900

    🌧 :: 청원 보기 api 추가

--- SVAP_Android/app/src/main/java/com/amazing/android/svap_android/api/PetitionApi.kt
+++ SVAP_Android/app/src/main/java/com/amazing/android/svap_android/api/PetitionApi.kt
@@ -1,13 +1,40 @@
 package com.amazing.android.svap_android.api
 
 import com.amazing.android.svap_android.feature.main.PopularPetitionResponse
+import com.amazing.android.svap_android.feature.showPetition.SortAllResponse
+import com.amazing.android.svap_android.feature.showPetition.SortPetitionResponse
+import com.amazing.android.svap_android.feature.showPetition.VoteAllResponse
+import com.amazing.android.svap_android.feature.showPetition.VoteListResponse
+import com.amazing.android.svap_android.type.AccessTypes
+import com.amazing.android.svap_android.type.Type
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface PetitionApi {
 
     @GET("/petition/popular")
     fun popularPetition(
     ): Call<PopularPetitionResponse>
+
+    @GET("/petition/vote/{type}")
+    fun voteList(
+        @Path("type") type: Type,
+    ): Call<List<VoteListResponse>>
+
+    @GET("petition/vote-all")
+    fun voteAll(
+    ): Call<List<VoteAllResponse>>
+
+    @GET("/petition/sort/{type}/{accessTypes}")
+    fun sortPetition(
+        @Path("type") type: Type,
+        @Path("accessTypes") accessTypes: AccessTypes,
+    ): Call<List<SortPetitionResponse>>
+
+    @GET("petition/sort-all/{accessTypes}")
+    fun sortPetitionAll(
+        @Path("accessTypes") accessTypes: AccessTypes,
+    ): Call<List<SortAllResponse>>
 }
