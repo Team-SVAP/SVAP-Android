7cee1ad3a86e0396441cbb2e9958c3ef4d72b6b1
parkUiery <parkuiery@gmail.com>
2023-11-30 23:02:07 +0900

    🌧 :: 아이템 디자인 변경

--- SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/write/TypeSpinnerAdapter.kt
+++ SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/write/TypeSpinnerAdapter.kt
@@ -7,13 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.annotation.LayoutRes
+import androidx.core.content.ContextCompat
+import com.amazing.android.svap_android.R
 import com.amazing.android.svap_android.databinding.PetitionTypeItemBinding
 
 class TypeSpinnerAdapter(
     context: Context,
     @LayoutRes private val resId: Int,
     private val values: MutableList<PetitionTypeModel>
-) : ArrayAdapter<PetitionTypeModel>(context,resId,values){
+) : ArrayAdapter<PetitionTypeModel>(context, resId, values) {
 
     override fun getCount() = values.size
 
@@ -21,17 +23,25 @@ class TypeSpinnerAdapter(
 
     @SuppressLint("ViewHolder")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val binding = PetitionTypeItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val binding =
+            PetitionTypeItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         val model = values[position]
         binding.tvPetitionType.text = model.name
         return binding.root
     }
 
     override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val binding = PetitionTypeItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val binding =
+            PetitionTypeItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         val model = values[position]
 
         binding.tvPetitionType.text = model.name
+
+        if (position == 0) {
+            binding.tvPetitionType.setTextColor(ContextCompat.getColor(context, R.color.gray_400))
+        } else {
+            binding.tvPetitionType.setTextColor(ContextCompat.getColor(context, R.color.gray_700))
+        }
         return binding.root
     }
 }
