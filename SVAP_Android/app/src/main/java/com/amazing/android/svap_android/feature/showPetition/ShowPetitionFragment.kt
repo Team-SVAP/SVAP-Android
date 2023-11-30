7a827e90495a23d775d37dae5bbd48385da913da
parkUiery <parkuiery@gmail.com>
2023-11-30 12:29:46 +0900

    🌧 :: 파일 변경

--- SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/showPetition/ShowPetitionFragment.kt
+++ SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/showPetition/ShowPetitionFragment.kt
@@ -1,60 +1,140 @@
 package com.amazing.android.svap_android.feature.showPetition
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.amazing.android.svap_android.R
+import com.amazing.android.svap_android.api.ApiProvider
+import com.amazing.android.svap_android.api.PetitionApi
+import com.amazing.android.svap_android.databinding.FragmentShowPetitionBinding
+import com.amazing.android.svap_android.type.AccessTypes
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ShowPetitionFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ShowPetitionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentShowPetitionBinding
+    private val retrofit: Retrofit = ApiProvider.getInstance()
+    private val petitionApi: PetitionApi = retrofit.create(PetitionApi::class.java)
+
+    private val listOfType = ArrayList<AccessTypesModel>()
+    private lateinit var typeAdapter: ShowSpinnerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_show_petition, container, false)
+    ): View {
+        binding = FragmentShowPetitionBinding.inflate(layoutInflater)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ShowPetitionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ShowPetitionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initData()
+        initSpinnerHandler()
+        initSpinner()
+        selectType()
+    }
+
+    private fun initSpinner() {
+        val recent = AccessTypesModel("최신순으로 보기", "recent")
+        listOfType.add(recent)
+
+        val vote = AccessTypesModel("투표순으로 보기", "vote")
+        listOfType.add(vote)
+
+        val approval = AccessTypesModel("승인된 청원 보기", "approval")
+        listOfType.add(approval)
+
+        val waiting = AccessTypesModel("검토중인 청원 보기", "waiting")
+        listOfType.add(waiting)
+
+        typeAdapter = ShowSpinnerAdapter(requireContext(), R.layout.petition_type_item, listOfType)
+        binding.spinnerShowPetition.adapter = typeAdapter
+    }
+
+    private fun initSpinnerHandler() {
+        binding.spinnerShowPetition.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    //
+                    val show =
+                        binding.spinnerShowPetition.getItemAtPosition(position) as AccessTypesModel
+                    when (show.type) {
+                        //"recent" -> //
+                        //"vote" -> //
+                        //"approval" -> //
+                        //"waiting" -> //
+
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    //
                 }
             }
+    }
+
+    private fun selectType() {
+        binding.apply {
+            tvShowPetitionAll.setOnClickListener {
+                changeType(
+                    tvShowPetitionAll,
+                    tvShowPetitionSchool,
+                    tvShowPetitionDormitory
+                )
+            }
+            tvShowPetitionDormitory.setOnClickListener {
+                changeType(
+                    tvShowPetitionDormitory,
+                    tvShowPetitionSchool,
+                    tvShowPetitionAll
+                )
+            }
+            tvShowPetitionSchool.setOnClickListener {
+                changeType(
+                    tvShowPetitionSchool,
+                    tvShowPetitionAll,
+                    tvShowPetitionDormitory
+                )
+            }
+        }
+    }
+
+    private fun changeType(select: TextView, other: TextView, another: TextView) {
+        select.setTextColor(resources.getColor(R.color.main_1))
+        other.setTextColor(resources.getColor(R.color.gray_400))
+        another.setTextColor(resources.getColor(R.color.gray_400))
+    }
+
+    private fun initData() {
+        petitionApi.sortPetitionAll(accessTypes = AccessTypes.NORMAL).enqueue(object :
+            Callback<List<SortAllResponse>> {
+            override fun onResponse(
+                call: Call<List<SortAllResponse>>,
+                response: Response<List<SortAllResponse>>
+            ) {
+                //
+                Log.d("TEST", "s" + response.body())
+            }
+
+            override fun onFailure(call: Call<List<SortAllResponse>>, t: Throwable) {
+                //
+            }
+        })
     }
 }
--- SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/showPetition/ShowSpinnerAdapter.kt
+++ SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/showPetition/ShowSpinnerAdapter.kt
@@ -1,4 +1,38 @@
 package com.amazing.android.svap_android.feature.showPetition
 
-class ShowSpinnerAdapter {
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.annotation.LayoutRes
+import com.amazing.android.svap_android.databinding.PetitionTypeItemBinding
+import com.amazing.android.svap_android.feature.write.PetitionTypeModel
+
+class ShowSpinnerAdapter(
+    context: Context,
+    @LayoutRes private val resId: Int,
+    private val values: MutableList<AccessTypesModel>
+) : ArrayAdapter<AccessTypesModel>(context,resId,values){
+
+    override fun getCount() = values.size
+
+    override fun getItem(position: Int) = values[position]
+
+    @SuppressLint("ViewHolder")
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val binding = PetitionTypeItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val model = values[position]
+        binding.tvPetitionType.text = model.name
+        return binding.root
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val binding = PetitionTypeItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val model = values[position]
+
+        binding.tvPetitionType.text = model.name
+        return binding.root
+    }
 }
