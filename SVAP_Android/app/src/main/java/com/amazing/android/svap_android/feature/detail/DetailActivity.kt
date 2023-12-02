957d07b3ea0ee03091663c1280707dfd7f980146
parkUiery <parkuiery@gmail.com>
2023-12-02 22:22:13 +0900

    🌧 :: 파일 추가

---
+++ SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.amazing.android.svap_android.feature.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.amazing.android.svap_android.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
