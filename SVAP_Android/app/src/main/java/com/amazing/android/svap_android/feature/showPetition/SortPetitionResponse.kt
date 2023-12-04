212f108d073b26e98352713d7f9a9304c4ec149a
parkUiery <parkuiery@gmail.com>
2023-12-04 23:30:35 +0900

    🌧 :: 이름 변경

--- SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/showPetition/SortPetitionResponse.kt
+++ SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/showPetition/SortPetitionResponse.kt
@@ -1,11 +1,11 @@
 package com.amazing.android.svap_android.feature.showPetition
 
-import com.amazing.android.svap_android.type.Type
+import com.amazing.android.svap_android.type.Types
 
 data class SortPetitionResponse(
     val id: Long,
     val title: String,
     val dateTime: String,
-    val types: Type,
+    val types: Types,
     val location: String,
 )
