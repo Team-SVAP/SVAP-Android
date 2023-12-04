c5a06aeea7855715df2ffbd8d570af02e051ece4
parkUiery <parkuiery@gmail.com>
2023-12-04 23:30:01 +0900

    🌧 :: 청원 작성 api

--- SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/write/WritePetitionRequest.kt
+++ SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/write/WritePetitionRequest.kt
@@ -1,4 +1,11 @@
 package com.amazing.android.svap_android.feature.write
 
-class WritePetitionRequest {
-}
+import com.amazing.android.svap_android.type.Types
+
+data class WritePetitionRequest (
+    val title: String,
+    val content: String,
+    val types: Types,
+    val location: String,
+    val imageUrl: List<String>,
+)
