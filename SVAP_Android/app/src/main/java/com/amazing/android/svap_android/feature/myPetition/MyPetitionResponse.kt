735e81edde7b9c7924ca6bc66c98f57228bb140e
parkUiery <parkuiery@gmail.com>
2023-12-05 20:17:17 +0900

    🌧 :: 이름 변경

--- SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/myPetition/MyPetitionResponse.kt
+++ SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/myPetition/MyPetitionResponse.kt
@@ -1,12 +1,12 @@
 package com.amazing.android.svap_android.feature.myPetition
 
-import com.amazing.android.svap_android.type.Type
+import com.amazing.android.svap_android.type.Types
 
 data class MyPetitionResponse(
     val id: String,
     val title: String,
     val content: String,
     val dateTime: String,
-    val types: Type,
+    val types: Types,
     val location: String,
 )
