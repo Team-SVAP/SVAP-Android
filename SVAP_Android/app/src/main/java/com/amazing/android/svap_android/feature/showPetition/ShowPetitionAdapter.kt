1231814cebcafaf9af94830974ad06cdc0007e68
parkUiery <parkuiery@gmail.com>
2023-12-07 21:58:15 +0900

    🌱 :: 청원 보기 어댑터 추가

--- SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/showPetition/ShowPetitionAdapter.kt
+++ SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/showPetition/ShowPetitionAdapter.kt
@@ -1,4 +1,40 @@
 package com.amazing.android.svap_android.feature.showPetition
 
-class ShowPetitionAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.amazing.android.svap_android.R
+import com.amazing.android.svap_android.type.Types
+
+class ShowPetitionAdapter(private val itemList: List<SortAllResponse>) :
+    RecyclerView.Adapter<ShowPetitionAdapter.ShowViewHolder>(){
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ShowPetitionAdapter.ShowViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.show_petition_item, parent, false)
+        return ShowViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ShowPetitionAdapter.ShowViewHolder, position: Int) {
+        holder.title.text = itemList[position].title
+        holder.time.text = itemList[position].dateTime
+        when (itemList[position].types) {
+            Types.SCHOOL -> holder.tag.text = "#학교_${itemList[position].location}"
+            Types.DORMITORY -> holder.tag.text = "#기숙사_${itemList[position].location}"
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    class ShowViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val title: TextView = itemView.findViewById(R.id.tv_show_petition_item_title)
+        val time: TextView = itemView.findViewById(R.id.tv_show_petition_item_date)
+        val tag: TextView = itemView.findViewById(R.id.tv_show_petition_item_tag)
+    }
 }
