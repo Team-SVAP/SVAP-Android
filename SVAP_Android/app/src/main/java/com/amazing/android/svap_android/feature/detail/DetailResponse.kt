6ded8a9846b45a89bdffce197d4f765d7681d81d
parkUiery <parkuiery@gmail.com>
2023-12-10 22:21:27 +0900

    🌱 :: 상세 보기 api

--- SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/detail/DetailResponse.kt
+++ SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/detail/DetailResponse.kt
@@ -1,3 +1,19 @@
 package com.amazing.android.svap_android.feature.detail
 
-data class DetailResponse()
+import com.amazing.android.svap_android.type.AccessTypes
+import com.amazing.android.svap_android.type.Types
+
+data class DetailResponse(
+    val id: Long,
+    val title: String,
+    val content: String,
+    val voteCounts: Int,
+    val accessTypes: AccessTypes,
+    val types: Types,
+    val location: String,
+    val viewCounts: Int,
+    val accountId: String,
+    val imgUrl: List<String>,
+    val dateTime: String,
+    val voted: Boolean,
+)
