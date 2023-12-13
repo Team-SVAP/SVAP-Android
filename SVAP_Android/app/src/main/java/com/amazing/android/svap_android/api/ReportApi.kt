f9b54e2279ab51ef044569fd2e6d778af4031a0f
parkUiery <parkuiery@gmail.com>
2023-12-13 18:22:06 +0900

    🌱 ::  신고 api 추가

--- SVAP_Android/app/src/main/java/com/amazing/android/svap_android/api/ReportApi.kt
+++ SVAP_Android/app/src/main/java/com/amazing/android/svap_android/api/ReportApi.kt
@@ -1,4 +1,13 @@
 package com.amazing.android.svap_android.api
 
+import retrofit2.Call
+import retrofit2.http.POST
+import retrofit2.http.Path
+
 interface ReportApi {
+
+    @POST("/svap/report/{petitionId}")
+    fun report(
+        @Path("petitionId") id: Long,
+    ): Call<Void>
 }
