ce962a01527a7afa392e852a4207fee16627dbf0
parkUiery <parkuiery@gmail.com>
2023-12-14 15:10:40 +0900

    🌧 :: 마이페이지 추가

--- SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/myPage/MyPageFragment.kt
+++ SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/myPage/MyPageFragment.kt
@@ -1,60 +1,128 @@
 package com.amazing.android.svap_android.feature.myPage
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.amazing.android.svap_android.R
+import com.amazing.android.svap_android.api.ApiProvider
+import com.amazing.android.svap_android.api.UserApi
+import com.amazing.android.svap_android.databinding.FragmentMyPageBinding
+import com.amazing.android.svap_android.feature.login.LoginActivity
+import com.amazing.android.svap_android.feature.myPetition.MyPetitionActivity
+import com.amazing.android.svap_android.util.CustomDialog
+import com.amazing.android.svap_android.util.DialogClickListener
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MyPageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MyPageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentMyPageBinding
+    private val retrofit: Retrofit = ApiProvider.getInstance()
+    private val userApi: UserApi = retrofit.create(UserApi::class.java)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_page, container, false)
+    ): View {
+        binding = FragmentMyPageBinding.inflate(layoutInflater)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MyPageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MyPageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initMovePage()
+        initUserData()
+        showDialog()
+    }
+
+    private fun showDialog() {
+        binding.tvMyPageLogout.setOnClickListener {
+            val logoutDialog = CustomDialog(
+                requireContext(),
+                "로그아웃 하시겠습니까?",
+                "다시 로그인 해야 합니다.",
+                "확인",
+                object : DialogClickListener {
+                    override fun onYesBtnClick() {
+                        logout()
+                    }
+                })
+            logoutDialog.show()
+        }
+
+        binding.tvMyPageDelete.setOnClickListener {
+            val deleteDialog = CustomDialog(
+                requireContext(),
+                "회원 탈퇴 하시겠습니까?",
+                "계정이 삭제됩니다.",
+                "확인",
+                object : DialogClickListener {
+                    override fun onYesBtnClick() {
+                        deleteUser()
+                    }
+                })
+            deleteDialog.show()
+        }
+    }
+
+    private fun logout() {
+        val intent = Intent(context, LoginActivity::class.java)
+        startActivity(intent)
+        activity?.finish()
+    }
+
+    private fun deleteUser() {
+        val accessToken = context?.getSharedPreferences("token", 0)?.getString("accessToken", null)
+        if (accessToken != null) {
+            userApi.deleteUser(accessToken = accessToken).enqueue(object : Callback<Void> {
+                override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                    when (response.code()) {
+                        200 -> {
+                            logout()
+                        }
+                    }
                 }
-            }
+
+                override fun onFailure(call: Call<Void>, t: Throwable) {
+                    Toast.makeText(context, R.string.fail_sever, Toast.LENGTH_SHORT).show()
+                }
+
+            })
+        }
+    }
+
+    private fun initUserData() {
+        val accessToken = context?.getSharedPreferences("token", 0)?.getString("accessToken", null)
+        if (accessToken != null) {
+            userApi.myInfo(accessToken = accessToken).enqueue(object : Callback<UserInfoResponse> {
+                override fun onResponse(
+                    call: Call<UserInfoResponse>,
+                    response: Response<UserInfoResponse>
+                ) {
+                    when (response.code()) {
+                        200 -> binding.tvMyPageName.text = response.body()?.userName
+                    }
+                }
+
+                override fun onFailure(call: Call<UserInfoResponse>, t: Throwable) {
+                    Toast.makeText(context, R.string.fail_sever, Toast.LENGTH_SHORT).show()
+                }
+
+            })
+        }
+    }
+
+    private fun initMovePage() {
+        binding.tvMyPageShow.setOnClickListener {
+            val intent = Intent(context, MyPetitionActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
