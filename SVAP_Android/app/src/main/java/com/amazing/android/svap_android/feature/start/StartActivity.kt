60b8359e3f5544e37568f5e04d9e7d0e58518944
parkUiery <parkuiery@gmail.com>
2023-12-19 14:16:50 +0900

    🌧 :: 시작화면 이동 추가

--- SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/start/StartActivity.kt
+++ SVAP_Android/app/src/main/java/com/amazing/android/svap_android/feature/start/StartActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.amazing.android.svap_android.databinding.ActivityStartBinding
 import com.amazing.android.svap_android.feature.login.LoginActivity
+import com.amazing.android.svap_android.feature.signup.SignUpActivity
 
 class StartActivity : AppCompatActivity() {
     private lateinit var binding: ActivityStartBinding
@@ -20,6 +21,10 @@ class StartActivity : AppCompatActivity() {
     private fun movePage() {
         binding.btnStart.setOnClickListener {
             val intent = Intent(baseContext, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        binding.tvStartGoSignUp.setOnClickListener {
+            val intent = Intent(baseContext, SignUpActivity::class.java)
             startActivity(intent)
         }
     }
